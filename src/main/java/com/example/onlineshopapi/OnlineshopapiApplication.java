b27d85ac5cbcaef191f5465c49af660df3747834
KasperFlodin <Kasperflodin@gmail.com>
2024-09-10 17:06:50 +0200

    Initial commit

---
+++ src/main/java/com/example/onlineshopapi/OnlineshopapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshopapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshopapiApplication.class, args);
+	}
+
+}
